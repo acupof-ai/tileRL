241371c53438949b60bc621974f818d6ba3d8537
cklxx <q1293822641@gmail.com>
2026-09-09 16:58:53 +0800

    Revert "fix(train): save completion text in rollout rows (#328)"
    This reverts commit 058152c2e613be0d5ca617bf1632e4ef82aebd51.

--- src/tilerl/cli.py
+++ src/tilerl/cli.py
@@ -1035,7 +1035,7 @@ def _train_adapters(args: argparse.Namespace) -> None:
                                     sampling=params, seed=args.seed,
                                     trainable=trainable, micro=args.micro,
                                     tiebreak=tiebreak, recapture_graph=True,
-                                    per_rollout=rollouts, decode=tok.decode)):
+                                    per_rollout=rollouts)):
             hist.append((r, ce, secs, tied, ntok))
             train_secs += secs
             written = _write_rollout_rows(manifest["id"], rollouts, written)
